30acf0c4574306925cdb79e143c757b2cc3f1c3d
hninshweyioo <hninshweyioo@localhost>
2022-01-02 19:39:19 +0800

    First Commit

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,129 @@
+package com.springboot.blog.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+	private PostRepository postRepository;
+	
+	public PostServiceImpl(PostRepository postRepository) {
+		
+		this.postRepository = postRepository;
+	}
+
+	@Override
+	public PostDto createPost(PostDto postDto) {
+		// TODO Auto-generated method stub
+		
+		//convert DTO to entity
+		Post post = mapToEntity(postDto);
+	
+		Post newPost = postRepository.save(post);
+		
+		//convert entity to DTO
+		PostDto postResponse = mapToDto(newPost);
+		
+		return postResponse;
+	}
+
+	@Override
+	public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+		// TODO Auto-generated method stub
+		
+		
+		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())?
+				Sort.by(sortBy).ascending(): Sort.by(sortBy).descending();
+		
+		//create pagable instance
+		Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+		
+		Page<Post> posts = postRepository.findAll(pageable);
+		
+		//get content for page object
+		List<Post> listOfPosts = posts.getContent();
+		
+		
+		List<PostDto> content = listOfPosts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+		
+		PostResponse postResponse = new PostResponse();
+		postResponse.setContent(content);
+		postResponse.setPageNo(posts.getNumber());
+		postResponse.setPageSize(posts.getSize());
+		postResponse.setTotalElements(posts.getTotalElements());
+		postResponse.setTotalPages(posts.getTotalPages());
+		postResponse.setLast(posts.isLast());
+		
+		
+		return postResponse;
+	}
+	
+	//convert Entity into DTO
+	private PostDto mapToDto(Post post) {
+		
+		PostDto postDto = new PostDto();
+		postDto.setId(post.getId());
+		postDto.setTitle(post.getTitle());
+		postDto.setDescription(post.getDescription());
+		postDto.setContent(post.getContent());
+		return postDto;
+	}
+	
+	//convert DTO to Entity
+	private Post mapToEntity(PostDto postDto) {
+			
+		Post post = new Post();
+		post.setTitle(postDto.getTitle());
+		post.setDescription(postDto.getDescription());
+		post.setContent(postDto.getContent());
+			
+		return post;
+	}
+
+	@Override
+	public PostDto getPostById(long id) {
+		// TODO Auto-generated method stub
+		Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException ("Post","id",id));
+		return mapToDto(post);
+	}
+
+	@Override
+	public PostDto updatePost(PostDto postDto, long id) {
+		// get post by id from database
+		Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException ("Post","id",id));
+		
+		post.setTitle(postDto.getTitle());
+		post.setDescription(postDto.getDescription());
+		post.setContent(postDto.getContent());
+		
+		Post updatedPost = postRepository.save(post);
+		
+		return mapToDto(updatedPost);
+	}
+
+	@Override
+	public void deletePostById(long id) {
+		// TODO Auto-generated method stub
+		// get post by id from database
+		Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException ("Post","id",id));
+		postRepository.delete(post);
+		
+		
+
+	}
+
+}
